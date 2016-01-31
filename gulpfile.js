aa094e6f9c36e570e5fc687bd918bde179c4d98e
Peter van Meijgaard <PetervanMeijgaard@hotmail.com>
2016-01-31 12:21:41 +0100

    Inialized for the SPA

--- gulpfile.js
+++ gulpfile.js
@@ -18,24 +18,73 @@ elixir(function(mix) {
     var directories = {
         javascript: {
             input: 'app.js',
-            output: 'public/js/app.js',
-            version: 'js/app.js'
+            output: 'public/js/app.js'
         },
         stylus: {
             input: 'vivid-finance.styl',
-            output: 'public/css/app.css',
-            version: 'css/app.css'
+            output: 'public/css/app.css'
+        },
+        images : {
+            input : 'resources/assets/img',
+            output : 'public/img'
         }
     };
 
     var version = [
-        directories.javascript.version,
-        directories.stylus.version
+        // CSS
+        'css/app.css',
+
+        // JS
+        'js/app.js',
+
+        // Fav Icons
+        'img/icons/favicon/favicon.ico',
+        'img/icons/favicon/favicon-16x16.png',
+        'img/icons/favicon/favicon-32x32.png',
+        'img/icons/favicon/favicon-96x96.png',
+        'img/icons/favicon/favicon-194x194.png',
+
+        // Android Icons
+        'img/icons/android/android-chrome-36x36.png',
+        'img/icons/android/android-chrome-48x48.png',
+        'img/icons/android/android-chrome-72x72.png',
+        'img/icons/android/android-chrome-96x96.png',
+        'img/icons/android/android-chrome-144x144.png',
+        'img/icons/android/android-chrome-192x192.png',
+        'img/icons/android/manifest.json',
+
+        // Apple Icons
+        'img/icons/apple/apple-touch-icon.png',
+        'img/icons/apple/apple-touch-icon-57x57.png',
+        'img/icons/apple/apple-touch-icon-60x60.png',
+        'img/icons/apple/apple-touch-icon-72x72.png',
+        'img/icons/apple/apple-touch-icon-76x76.png',
+        'img/icons/apple/apple-touch-icon-114x114.png',
+        'img/icons/apple/apple-touch-icon-120x120.png',
+        'img/icons/apple/apple-touch-icon-144x144.png',
+        'img/icons/apple/apple-touch-icon-152x152.png',
+        'img/icons/apple/apple-touch-icon-180x180.png',
+        'img/icons/apple/apple-touch-icon-precomposed.png',
+
+        // Microsoft Icons
+        'img/icons/microsoft/mstile-70x70.png',
+        'img/icons/microsoft/mstile-144x144.png',
+        'img/icons/microsoft/mstile-150x150.png',
+        'img/icons/microsoft/mstile-310x150.png',
+        'img/icons/microsoft/mstile-310x310.png',
+        'img/icons/microsoft/browserconfig.xml',
+
+        // Safari Icons
+        'img/icons/safari/safari-pinned-tab.svg',
+
+        // Logos
+        'img/logo/logo.svg'
     ];
 
     mix
         .browserify(directories.javascript.input, directories.javascript.output)
         .stylus(directories.stylus.input, directories.stylus.output)
+        .copy(directories.images.input, directories.images.output)
         .version(version)
         .phpUnit()
         .phpSpec()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,58 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Vivid Finance</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="author" content="Vivid Finance">
+        <meta name="description"
+              content="Vivid Finance. Easy to use software to manage your finance">
+        <meta name="keywords"
+              content="PHP, Vivid, Finance, Laravel, Vue">
+        <meta name="robots" content="index, follow">
+        <meta name="revisit-after" content="1 month">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <meta name="theme-color" content="#009ade">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <!-- Fav Icon Configuration -->
+        <link rel="shortcut icon" href="{{ elixir('img/icons/favicon/favicon.ico') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ elixir('img/icons/favicon/favicon-16x16.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ elixir('img/icons/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ elixir('img/icons/favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="194x194" href="{{ elixir('img/icons/favicon/favicon-194x194.png') }}">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+        <!-- Android Configuration -->
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ elixir('img/icons/android/android-chrome-192x192.png') }}">
+        <link rel="manifest" href="{{ elixir('img/icons/android/manifest.json') }}">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="application-name" content="Vivid Finance">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+        <!-- Apple Configuration -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#009ade">
+        <meta name="apple-mobile-web-app-title" content="Vivid Finance">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ elixir('img/icons/apple/apple-touch-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ elixir('img/icons/apple/apple-touch-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ elixir('img/icons/apple/apple-touch-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ elixir('img/icons/apple/apple-touch-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ elixir('img/icons/apple/apple-touch-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ elixir('img/icons/apple/apple-touch-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ elixir('img/icons/apple/apple-touch-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ elixir('img/icons/apple/apple-touch-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ elixir('img/icons/apple/apple-touch-icon-180x180.png') }}">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+        <!-- Microsoft Configuration -->
+        <meta name="msapplication-TileColor" content="#009ade">
+        <meta name="msapplication-TileImage" content="{{ elixir('img/icons/microsoft/mstile-144x144.png') }}">
+        <meta name="msapplication-config" content="{{ elixir('img/icons/microsoft/browserconfig.xml') }}">
+        <meta name="msapplication-tap-highlight" content="no">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <!-- Safari Configuration -->
+        <link rel="mask-icon" href="{{ elixir('img/icons/safari/safari-pinned-tab.svg') }}" color="#009ade">
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}"/>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+        <div id="app"></div>
+        <script src="{{ elixir('js/app.js') }}" async defer></script>
     </body>
 </html>
