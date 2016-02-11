a834a2b6879dbcb57989199d2380ce8a75fff990
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-02-11 17:37:06 +0100

    Adds Invoice index

--- resources/assets/js/views/invoice/index/index.js
+++ resources/assets/js/views/invoice/index/index.js
@@ -1,2 +1,68 @@
+import store from '../../../store'
+const { getAllInvoices } = store.actions;
+
 export default {
+    data () {
+        return {
+            breadcrumb: [
+                {
+                    title: 'Home',
+                    link: 'invoice.index'
+                },
+                {
+                    title: 'Invoices',
+                    link: 'invoice.index'
+                },
+                {
+                    title: 'Invoice Overview'
+                }
+            ],
+            table: {
+                columns: [
+                    {
+                        'title': 'Name',
+                        'slug': 'name'
+                    },
+                    {
+                        'title': 'Customer number',
+                        'slug': 'id'
+                    },
+                    {
+                        'title': 'Email address',
+                        'slug': 'email'
+                    },
+                    {
+                        'title': 'Phone number',
+                        'slug': 'telephone'
+                    }
+                ],
+                notFound: 'No invoices yet...',
+                action (customer) {
+                    console.log(customer);
+                }
+            },
+            vPagination : {
+                fn : getAllInvoices
+            }
+        }
+    },
+    computed: {
+        invoices () {
+            return store.state.invoices.data
+        },
+        pagination () {
+            return store.state.invoices.pagination
+        }
+    },
+    components: {
+        'v-breadcrumb' (resolve) {
+            require(['./../../../components/Breadcrumb.vue'], resolve)
+        },
+        'v-table' (resolve) {
+            require(['./../../../components/Table.vue'], resolve)
+        },
+        'v-pagination' (resolve) {
+            require(['./../../../components/Pagination.vue'], resolve)
+        }
+    }
 };
