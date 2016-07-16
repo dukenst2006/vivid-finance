794a29ffaa7ea04465c5ff05edbf8e3b76c32357
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-07-16 14:01:53 +0200

    Update

--- resources/frontend/src/app/components/User/Figure/figure.vue
+++
@@ -1,3 +0,0 @@
-<style src="./figure.styl" lang="stylus"></style>
-<template src="./figure.html"></template>
-<script src="./figure.js" lang="babel"></script>
--- resources/frontend/src/app/components/menu/menu.vue
+++
@@ -1,3 +0,0 @@
-<style src="./menu.styl" lang="stylus"></style>
-<template src="./menu.html"></template>
-<script src="./menu.js" lang="babel"></script>
