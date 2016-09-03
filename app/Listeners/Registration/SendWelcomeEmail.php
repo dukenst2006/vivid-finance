56800f6443e2617a1a2013770532f5cc4a1fe094
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-09-03 18:13:33 +0200

    Adds database Queue table

--- app/Listeners/Registration/SendWelcomeEmail.php
+++ app/Listeners/Registration/SendWelcomeEmail.php
@@ -4,9 +4,10 @@ namespace App\Listeners\Registration;
 
 use App\Events\User\Registered;
 use App\Mail\Registration\Welcome;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Mail;
 
-class SendWelcomeEmail
+class SendWelcomeEmail implements ShouldQueue
 {
 
     /**
@@ -29,7 +30,6 @@ class SendWelcomeEmail
      */
     public function handle(Registered $event)
     {
-        Mail::to($event->user->email)
-            ->send(new Welcome($event->user));
+        Mail::to($event->user->email)->send(new Welcome($event->user));
     }
 }
